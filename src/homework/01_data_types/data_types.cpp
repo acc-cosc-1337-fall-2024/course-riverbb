8d279ae4dcc38537308491156c8dea4eda5aa2c4
riverbb <157077928+riverbb@users.noreply.github.com>
2024-02-10 20:28:29 -0600

    Update data_types.cpp

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -1,13 +1,21 @@
 #include "data_types.h"
 
 //example
-int add_numbers(int num1, int num2)
-{
-	return num1 + num2;
-}
+//int add_numbers(int num1, int num2)
+//{
+//	return num1 + num2;
+//}
 
 //write function code here
 
+int multiply_numbers(int num1)
+{
+  int num2 = 5;
+
+  int return_value = num1 * num2;
+
+  return return_value;
+}
 
 
 
