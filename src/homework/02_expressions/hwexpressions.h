70402eae8b8b3a29cc40c71885d48a4e2d6bfcb7
riverbb <157077928+riverbb@users.noreply.github.com>
2024-02-10 19:43:44 -0600

    Update hwexpressions.h

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,8 +1,14 @@
-﻿//example
+//example
 int add_numbers(int num1, int num2);
+
+//declarations
+const double TAX_RATE = 6.75;
 
 //write function prototype here
 
+//1: create a double value-return function prototype named get_sales_tax_amount with one double parameter: meal_amount.
+double get_sales_tax_amount(double meal_amount);
 
 
-
+//3: create a double value-return function prototype named get_tip_amount with two double parameters: meal_amount and tip_rate.
+double get_tip_amount(double meal_amount, double tip_rate);
