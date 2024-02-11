85c6f489ec79e98b458e7c4b7c955d89f90f7685
riverbb <157077928+riverbb@users.noreply.github.com>
2024-02-10 19:42:55 -0600

    Update hwexpressions.cpp

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,13 +1,23 @@
 #include "hwexpressions.h"
+#include <iostream>
 
 //example
-int add_numbers(int num1, int num2)
-{
-	return num1 + num2;
-}
+//int add_numbers(int num1, int num2)
+//{
+//	return num1 + num2;
+//}
 
 //write function code here
 
+//2: write function code, multiply tax_rate and meal_amount and return their product.
+double get_sales_tax_amount(double meal_amount)
+  {
+    return meal_amount * (TAX_RATE / 100.0);
+  }
 
-
+//4: write function code to multiply  meal_amount and tip_rate, return their product.
+double get_tip_amount(double meal_amount, double tip_rate)
+  {
+    return meal_amount * (tip_rate / 100.0);
+  }
 
