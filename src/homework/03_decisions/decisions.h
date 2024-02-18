9811030933bf2463c026e2f4ce685f9148145a66
riverbb <157077928+riverbb@users.noreply.github.com>
2024-02-17 19:23:48 -0600

    Update decisions.h

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,8 +1,13 @@
+// 1. write function prototypes for get_letter_grade_using_if and get_letter_grade_using_switch.
+
 //write include required statements
 
+#define DECISIONS_H
+
+#include <string>
 
 //Write a function(s) prototype  here
 
+std::string get_letter_grade_using_if(int grade);
 
-
-
+std::string get_letter_grade_using_switch(int grade);
