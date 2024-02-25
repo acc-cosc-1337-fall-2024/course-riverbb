b732b4c2124d28a19c16ea43ae2e9137b662b971
riverbb <157077928+riverbb@users.noreply.github.com>
2024-02-24 21:43:11 -0600

    Update repetition.cpp

--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,33 @@
+/*
+In file  src/homework/04_repetition /repetition .cpp, write the function code for the factorial and gcd functions using a loop of your choice. Do not use a recursive solution to solve this problem.
+*/
+
 //add include statements
+#include "repetition.h"
+#include <iostream>
 
 //add function(s) code here
+int factorial(int num)
+{
+  int sum = 1;
+  while (num > 0) {
+    sum *= num;
+    num--;
+  }
+  return sum;
+}
+
+
+
+int gcd(int num1, int num2)
+{
+  while (num1 != num2) {
+    if (num1 < num2){
+      std::swap(num1, num2);
+    }
+    if (num1 > num2) {
+      num1 -= num2;
+    }
+  }
+  return num1;
+}
