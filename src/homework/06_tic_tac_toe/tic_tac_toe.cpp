7d537edd8a0e79774a186e3e25a4f8b344119514
riverbb <157077928+riverbb@users.noreply.github.com>
2024-04-06 19:33:21 -0500

    Update tic_tac_toe.cpp

--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,11 +1,39 @@
 //cpp
 #include "tic_tac_toe.h"
-
 using std::cout;
+
+////////////////////////////////////////////////////////////////////////
+//public functions//////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 bool TicTacToe::game_over()
 {
-  return check_board_full();
+  /*
+  update class function to determine a win by checking for row, column, or diagonal win. (use conditional structure), 
+  Example:
+  if winner by row, column, or diagonal
+  call the set winner function and return true
+  else
+  check board full equal true indicates a tie 
+    If no winner by row, column, or diagonal set   winner variable to C and return true.
+  else
+  return false-if no winner n board not full
+  */
+
+  if(check_row_win() || check_column_win() || check_diagonal_win())
+  {
+    set_winner();
+    return true;
+  }
+  else if (check_board_full())
+  {
+    winner = 'C';
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 void TicTacToe::start_game(std::string first_player)
@@ -28,7 +56,10 @@ void TicTacToe::display_board() const
     }
 }
 
-//private functions
+////////////////////////////////////////////////////////////////////////
+//private functions/////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
 void TicTacToe::clear_board()
 {
   for(auto& peg: pegs)
@@ -60,4 +91,61 @@ bool TicTacToe::check_board_full()
     }
 
   return true;
+}
+
+//NEW//////////////////////////////////////////////////////////////
+
+bool TicTacToe::check_column_win()
+{
+  //column wins 1,4,7 or 2,5,8, or 3,6,9 with all Os or Xs
+  for (int i=0; i<3; i++)
+    {
+      if (pegs[i] != " " && pegs[i] == pegs[i+3] && pegs[i] == pegs[i+6])
+      {
+        return true;
+      }
+    }
+  return false;
+}
+
+bool TicTacToe::check_row_win()
+{
+  //row wins 1,2,3 or 4,5,6 or 7,8,9 with all Os or Xs
+  for (int i=0; i<9; i+=3)
+    {
+      if (pegs[i] != " " && pegs[i] == pegs[i+1] && pegs[i] == pegs[i+2])
+      {
+        return true;
+      }
+    }
+  return false;
+}
+
+bool TicTacToe::check_diagonal_win()
+{
+  //diagonal wins 1,5,9 or 3,5,7 with all Os or Xs
+  if (pegs[0] != " " && pegs[0] == pegs[4] && pegs[0] == pegs[8])
+  {
+    return true;
+  }
+
+  if (pegs[2] != " " && pegs[2] == pegs[4] && pegs[2] == pegs[6])
+  {
+    return true;
+  }
+  
+  return false;
+}
+
+void TicTacToe::set_winner()
+{
+  //If player is X set winner to O otherwise set winner to X
+  if(player == "X")
+  {
+    winner = "O";
+  }
+  else
+  {
+    winner = "X";
+  }
 }
