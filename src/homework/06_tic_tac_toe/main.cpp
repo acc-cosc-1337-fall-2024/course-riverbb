561efbda6cd92f6afaf930366474b026e645fcec
riverbb <157077928+riverbb@users.noreply.github.com>
2024-04-13 21:45:35 -0500

    Update main.cpp

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,4 +1,5 @@
 #include "tic_tac_toe.h"
+#include "tic_tac_toe_manager.h"
 
 #include <iostream>
 #include <string>
@@ -7,6 +8,7 @@ using std::cout; using std::cin; using std::string;
 
 int main() 
 {
+  TicTacToeManager manager;
   TicTacToe game;
   string first_player;
   char user_choice = 'y';
@@ -43,6 +45,12 @@ int main()
       {
         cout << game.get_winner() << " wins! :D \n";
       }
+
+      manager.save_game(game);
+
+      int x_win, o_win, ties;
+      manager.get_winner_total(x_win, o_win, ties);
+      cout << "\nrunning scores \n" << "x wins: " << x_win << "\n" << "o wins: " << o_win << "\n" << "ties: " << ties << "\n \n";
 
         cout << "play again? (y/n): ";
         cin >> user_choice;
