cf5faa0a2a94f865c5d6bb9961ba81d3509cf7b9
riverbb <157077928+riverbb@users.noreply.github.com>
2024-04-13 21:49:49 -0500

    Update tic_tac_toe_tests.cpp

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,6 +1,9 @@
+/*
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "tic_tac_toe.h"
+#include "tic_tac_toe_manager.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
   REQUIRE(true == true);
@@ -32,8 +35,6 @@ TEST_CASE("Test game over if 9 slots are selected.")
   REQUIRE(game.game_over() == true);
   REQUIRE(game.get_winner() == "C");
 }
-
-//NEW//////////////////////////////////////////////////////////////
 
 TEST_CASE("Test first player set to X")
 {
@@ -207,4 +208,48 @@ TEST_CASE("Test win diagonally from bottom left")
 
   REQUIRE(game.game_over() == true);
   REQUIRE(game.get_winner() == "X");
+}
+
+////////////////////////////////////////
+
+TEST_CASE("Test tic_tac_toe_manager get winner total") {
+    TicTacToeManager manager;
+    TicTacToe game1, game2, game3;
+
+    // simulating games with different winners
+    game1.start_game("X");
+    game1.mark_board(1); // X
+    game1.mark_board(2); // O
+    game1.mark_board(4); // X
+    game1.mark_board(5); // O
+    game1.mark_board(7); // X
+
+    game2.start_game("O");
+    game2.mark_board(1); // O
+    game2.mark_board(4); // X
+    game2.mark_board(2); // O
+    game2.mark_board(5); // X
+    game2.mark_board(3); // O
+
+    game3.start_game("X");
+    game3.mark_board(1); // X
+    game3.mark_board(2); // O
+    game3.mark_board(3); // X
+    game3.mark_board(4); // O
+    game3.mark_board(5); // X
+    game3.mark_board(7); // O
+    game3.mark_board(6); // X
+    game3.mark_board(9); // O
+    game3.mark_board(8); // X
+
+    manager.save_game(game1);
+    manager.save_game(game2);
+    manager.save_game(game3);
+
+    int x_win, o_win, ties;
+    manager.get_winner_total(o_win, x_win, ties);
+
+    REQUIRE(x_win == 1);
+    REQUIRE(o_win == 1);
+    REQUIRE(ties == 1);
 }
