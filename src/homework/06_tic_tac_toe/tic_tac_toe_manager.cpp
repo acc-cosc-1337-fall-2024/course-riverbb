e8040e9b289fd46c3cca348fc5773f7e95ff32b3
riverbb <157077928+riverbb@users.noreply.github.com>
2024-04-13 21:42:13 -0500

    Update tic_tac_toe_manager.cpp

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,1 +1,46 @@
 //cpp
+#include "tic_tac_toe_manager.h"
+using std::cout;
+
+////////////////////////////////////////////////////////////////////////
+//public functions//////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+void TicTacToeManager::save_game(TicTacToe game)
+{
+  //add the TicTacToe to games vector with push_back, call update winner count pass the winner from TicTacToe to update x, o, or tie totals.
+  games.push_back(game);
+
+  update_winner_count(game.get_winner());
+}
+
+void TicTacToeManager::get_winner_total(int& x, int& o, int& t)
+{
+  //Use references to get the winners 
+
+  x = x_win;
+  o = o_win;
+  t = ties;
+}
+
+////////////////////////////////////////////////////////////////////////
+//private functions/////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+void TicTacToeManager::update_winner_count(std::string winner)
+{
+  //if winner X increment x_win, if winner O increment o_win, and else increment ties
+
+  if (winner == "X")
+  {
+    x_win++;
+  }
+  else if (winner == "O")
+  {
+    o_win++;
+  }
+  else
+  {
+    ties++;
+  }
+}
